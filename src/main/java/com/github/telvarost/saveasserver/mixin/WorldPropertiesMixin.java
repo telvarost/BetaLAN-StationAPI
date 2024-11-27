e96fa8a7440a60f8931fe4ffd5478ceb9c153e12
telvarost <telontelvarost@gmail.com>
2024-11-26 20:23:12 -0500

    Hunt out files that need to change

---
+++ src/main/java/com/github/telvarost/saveasserver/mixin/WorldPropertiesMixin.java
@@ -0,0 +1,9 @@
+package com.github.telvarost.saveasserver.mixin;
+
+import net.minecraft.world.WorldProperties;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(WorldProperties.class)
+public class WorldPropertiesMixin {
+
+}
---
+++ src/main/java/com/github/telvarost/saveasserver/mixin/server/MinecraftServerMixin.java
@@ -0,0 +1,8 @@
+package com.github.telvarost.saveasserver.mixin.server;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+}
