0c001c37b350cd2de73af5d3af053814b260ba97
telvarost <telontelvarost@gmail.com>
2024-11-28 11:33:56 -0500

    Attempt to setup client server properties

--- src/main/java/com/github/telvarost/saveasserver/mixin/client/PauseMixin.java
+++ src/main/java/com/github/telvarost/saveasserver/mixin/client/PauseMixin.java
@@ -6,6 +6,7 @@ import io.github.prospector.modmenu.gui.ModMenuButtonWidget;
 import io.github.prospector.modmenu.mixin.MixinGuiButton;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.*;
 import net.minecraft.client.gui.widget.ButtonWidget;
@@ -22,8 +23,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
+import java.util.Scanner;
 
 @Environment(EnvType.CLIENT)
 @Mixin(GameMenuScreen.class)
@@ -104,14 +108,112 @@ public class PauseMixin extends Screen {
             this.minecraft.setScreen(new TitleScreen());
 
             /** - Edit server properties */
-            // Change level to client world
-            // Change port to configured port
+            File serverPropertiesFile = new File(Minecraft.getRunDirectory(), "server.properties");
+            if (!serverPropertiesFile.exists())
+            {
+                try {
+                    serverPropertiesFile.createNewFile();
+                    FileWriter writer = new FileWriter(serverPropertiesFile);
+                    writer.write("#Minecraft server properties\n");
+                    writer.write("#" + new Date() + "\n");
+                    if (null != ModHelper.ModHelperFields.CurrentWorldFolder) {
+                        writer.write("level-name=./saves/" + ModHelper.ModHelperFields.CurrentWorldFolder + "\n");
+                    } else {
+                        writer.write("#level-name=world\n");
+                    }
+                    writer.write("default-gamemode=0\n");
+                    writer.write("view-distance=10\n");
+                    writer.write("white-list=false\n");
+                    writer.write("server-ip=\n");
+                    writer.write("pvp=true\n");
+                    writer.write("level-seed=\n");
+                    writer.write("spawn-animals=true\n");
+                    writer.write("server-port=" + Config.config.SERVER_PORT + "\n");
+                    writer.write("allow-nether=true\n");
+                    writer.write("spawn-monsters=tru\n");
+                    writer.write("max-players=20\n");
+                    writer.write("online-mode=false\n");
+                    writer.write("allow-flight=false\n");
+                } catch (FileNotFoundException e) {
+                    System.out.println("Failed to create LAN server properties: ");
+                    e.printStackTrace();
+                }
+            } else {
+                File tempServerPropertiesFile = new File(Minecraft.getRunDirectory(), "_server.properties");
+                Files.copy(serverPropertiesFile.toPath(), tempServerPropertiesFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                try {
+                    FileWriter writer = new FileWriter(tempServerPropertiesFile);
+                    Reader reader = new FileReader(serverPropertiesFile);
+                    BufferedReader br = new BufferedReader(reader);
+
+                    while(br.ready()) {
+                        String currentLine = br.readLine();
+
+                        // Change level to client world
+                        if (currentLine.contains("level-name")) {
+                            if (null != ModHelper.ModHelperFields.CurrentWorldFolder) {
+                                writer.write("level-name=./saves/" + ModHelper.ModHelperFields.CurrentWorldFolder + "\n");
+                            }
+                        }
+
+                        // Change port to configured port
+                        if (currentLine.contains("server-port")) {
+                            if (null != ModHelper.ModHelperFields.CurrentWorldFolder) {
+                                writer.write("server-port=" + Config.config.SERVER_PORT + "\n");
+                            }
+                        }
+
+                        // Change to offline mode
+                        if (currentLine.contains("online-mode")) {
+                            if (null != ModHelper.ModHelperFields.CurrentWorldFolder) {
+                                writer.write("online-mode=false\n");
+                            }
+                        }
+                    }
+
+                    writer.close();
+                    br.close();
+                    reader.close();
+                    tempServerPropertiesFile.renameTo(serverPropertiesFile);
+                } catch (FileNotFoundException e) {
+                    System.out.println("Failed to update LAN server properties: ");
+                    e.printStackTrace();
+                }
+                if (serverPropertiesFile.exists() && tempServerPropertiesFile.exists()) {
+                    tempServerPropertiesFile.delete();
+                }
+            }
 
             /** - Launch server */
             String argNoGui = (Config.config.SERVER_GUI_ENABLED) ? "" : "nogui";
             ProcessBuilder pb = new ProcessBuilder(Config.config.JAVA_PATH, "-jar", "local-babric-server.0.16.9.jar", argNoGui);
             pb.directory(Minecraft.getRunDirectory());
             ModHelper.ModHelperFields.CurrentServer = pb.start();
+
+            /** - Monitor server to see when world is ready */
+            // Also need to create a loading screen to display monitored data
+//            new Thread(new Runnable() {
+//                public void run() {
+//                    try {
+//                        RandomAccessFile in = new RandomAccessFile("server.log", "r");
+//                        String line;
+//                        while (true) {
+//                            if ((line = in.readLine()) != null) {
+//                                System.out.println(line);
+//                            } else {
+//                                Thread.sleep(1000); // poll the file every 1 second
+//                            }
+//                        }
+//                    } catch (FileNotFoundException e) {
+//                        throw new RuntimeException(e);
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    } catch (IOException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                }
+//            }).start();
+
             Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
                 public void run() {
                     if (null != ModHelper.ModHelperFields.CurrentServer) {
